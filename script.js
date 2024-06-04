0f69993ececb0079d772932c0b4338308302fbe1
monamx <129038957+monamx@users.noreply.github.com>
2024-06-05 05:10:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -44,8 +44,8 @@ const puppeteer = require('puppeteer');
     const titles = await page.title();
     console.log(`Meta titles halaman: ${titles}`);
 
-    const adsenseElement = await page.$('ins.adsbygoogle');
-    if (adsenseElement) {
+    const adsenseElements = await page.$('ins.adsbygoogle');
+    if (adsenseElements) {
       console.log('Iklan AdSense dimuat di halaman.');
     } else {
       console.log('Tidak ada iklan AdSense yang dimuat di halaman.');
